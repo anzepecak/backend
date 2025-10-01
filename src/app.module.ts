815e7aeb55eacc61e85604390177413d05742d98
anzepecak <130110241+anzepecak@users.noreply.github.com>
2025-10-01 10:25:12 +0200

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigModule, ConfigType} from '@nestjs/config';
+import * as process from "node:process";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({isGlobal: true}),
+      TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
